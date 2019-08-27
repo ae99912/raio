73423f6ca8e03837ae70f0cc9f63f255c328d170
ae711e <ae711e@gmail.com>
2019-08-27 14:12:08 +0300

    переименовал

---
+++ zoo/table raio_oktmo.sql
@@ -0,0 +1,19 @@
+-- phpMyAdmin SQL Dump
+-- Время создания: Авг 20 2019 г., 15:26
+-- Версия PHP: 5.6.32
+--
+-- База данных: `raio`
+--
+-- --------------------------------------------------------
+-- Структура таблицы `raio_oktmo`
+--
+
+CREATE TABLE `raio_oktmo` (
+  `oktmo`   varchar(32)   PRIMARY KEY COMMENT 'код ОКТМО региона',
+  `okato`   varchar(32)   COMMENT 'код ОКАТО',
+  `zapros`  varchar(255)  COMMENT 'строка запроса к nominatim.openstreetmap.org',
+  `geojson` text          COMMENT 'строка JSON полигона',
+  `kratko`  varchar(128)  COMMENT 'краткое название региона',
+  `wdat`    timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'время записи'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='гео данные регионов ОКТМО';
+
