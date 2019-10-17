828d22544e76e6aacde20c2aa1b8e649afacb2d4
ae711e <ae711e@gmail.com>
2019-10-17 16:03:52 +0300

    ред.

--- t.js
+++ t.js
@@ -19,17 +19,18 @@
 
  */
 
-var strRegs = '?regs=';           // аргумент параметров под-"регионы"
-var Map1;
-var colorSelect  = '#aa1314';
-var colorNoselect = '#3f3fa2';
-var strKeyMetka = '0305396879554012335'; // ключевая метка для наших полигонов
-var Regions = defineRegion();  // определить регионы картирования (2 цифры)
-var Regions0 = Regions.substr(0,2); // регион или список регионов;     // начальный набор
-var promise_regions = [];   // массив обещаний
-var MyBounds = [[],[]];     // границы регионов
-//
-ymaps.ready(f1);
+// var strRegs = '?regs=';           // аргумент параметров под-"регионы"
+// var Map1;
+// var colorSelect  = '#aa1314';
+// var colorNoselect = '#3f3fa2';
+// var strKeyMetka = '0305396879554012335'; // ключевая метка для наших полигонов
+// var Regions = defineRegion();  // определить регионы картирования (2 цифры)
+// var Regions0 = Regions.substr(0,2); // регион или список регионов;     // начальный набор
+// var promise_regions = [];   // массив обещаний
+// var MyBounds = [[],[]];     // границы регионов
+// //
+
+// ymaps.ready(f1);
 
 function f1() {
   // 0. Создаем карту, например так:
